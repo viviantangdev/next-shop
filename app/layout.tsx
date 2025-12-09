6dde392361a8072b38a58c19341e62c34443d3b2
Vivian Tang <viviantang.dev@gmail.com>
2025-12-09 18:03:20 +0100

    Simple footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Navbar from '../components/navbar/Navbar';
 import './globals.css';
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
         <Navbar />
         {children}
+        <Footer/>
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return     <footer className='bg-amber-500 mx-auto p-4'>
+      <h2>NextShop</h2>
+      <span className='text-xs'>
+        Copyrigth &copy; {new Date().getFullYear()} Vivian Tang. All rigths
+        reserved
+      </span>
+    </footer>
+}
